e318832c75274c7744b4b5b81542384ed22fa446
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-11-30 09:00:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,231 +1,207 @@
- const {
-  default: makeWASocket,
-  Browsers,
-  makeInMemoryStore,
-  useMultiFileAuthState,
-} = require("@whiskeysockets/baileys");
-const fs = require("fs");
-const { serialize } = require("./lib/serialize");
-const { Message, Image, Sticker } = require("./lib/Base");
+const fs = require("fs").promises;
 const pino = require("pino");
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  Browsers,
+  delay,
+  DisconnectReason,
+  makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+const { PausedChats } = require("./lib/database");
+require("events").EventEmitter.defaultMaxListeners = 15;
 const path = require("path");
-const events = require("./lib/events");
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 8000;
-const got = require("got");
+const { Image, Message, Sticker, Video } = require("./lib/Base");
 const config = require("./config");
-const Config = require("./config");
-const { PluginDB } = require("./lib/db/plugins");
-const Greetings = require("./lib/Greetings");
-const { async } = require("q");
-const { getStatus, getMessage } = require("./lib/db/greetings");
-const { decodeJid, parseJid, getBuffer } = require("./lib");
-const store = makeInMemoryStore({
-  logger: pino().child({ level: "silent", stream: "store" }),
-});
-function decrypt(session){
-    let b = session.split("")
-    let c = "",l="",d="",t;
-    b.map((m)=>{
-        if(c.length<5){
-            c += m;
-        } else {
-            l = session.replace(c,'');
-        }
-        let q = l.split("");
-        q.map((r)=>{
-            if(d.length < 4 ){
-                d += r; 
-            }
-        })
-    })
-    t = c + session.replace(c,'').replace(d,'');
-    return t;
-    }
-let plaintext = config.SESSION_ID.replaceAll("jsl~", "");
-let session = decrypt(plaintext);
-const axios = require("axios");
-function _0x5570(){const _0x3eeb67=['exit','existsSync','831978qedzNU','SESSION_ID','split','4064800bpssjC','replaceAll','8ahGZOz','map','./lib/auth_info_baileys','test','content','./lib/auth_info_baileys/creds.json','1574037eJtTWJ','log','length','1999767FKwJAP','3NtVIuK','jsl~','1085765hHeTbT','511322bYKCNr','replace','4KODrbZ','please\x20provide\x20a\x20session\x20id\x20in\x20config.js\x0a\x0ascan\x20from\x20inrl\x20server','486679NQDjhc','30OvciEf','axios','files'];_0x5570=function(){return _0x3eeb67;};return _0x5570();}const _0x204d73=_0x31b2;(function(_0x137a67,_0x591b92){const _0x4ec8f0=_0x31b2,_0x209ca2=_0x137a67();while(!![]){try{const _0x9ba7cd=parseInt(_0x4ec8f0(0x13a))/0x1+-parseInt(_0x4ec8f0(0x140))/0x2*(-parseInt(_0x4ec8f0(0x133))/0x3)+-parseInt(_0x4ec8f0(0x138))/0x4*(parseInt(_0x4ec8f0(0x135))/0x5)+parseInt(_0x4ec8f0(0x13b))/0x6*(parseInt(_0x4ec8f0(0x136))/0x7)+parseInt(_0x4ec8f0(0x129))/0x8*(-parseInt(_0x4ec8f0(0x12f))/0x9)+-parseInt(_0x4ec8f0(0x143))/0xa+-parseInt(_0x4ec8f0(0x132))/0xb;if(_0x9ba7cd===_0x591b92)break;else _0x209ca2['push'](_0x209ca2['shift']());}catch(_0xf8322d){_0x209ca2['push'](_0x209ca2['shift']());}}}(_0x5570,0x46357));function _0x31b2(_0x924fa9,_0x13eab9){const _0x55700f=_0x5570();return _0x31b2=function(_0x31b2f1,_0x3cdf62){_0x31b2f1=_0x31b2f1-0x128;let _0x171055=_0x55700f[_0x31b2f1];return _0x171055;},_0x31b2(_0x924fa9,_0x13eab9);}function decrypt(_0x314579){const _0x2c9ba4=_0x31b2;let _0x42b7cf=_0x314579['split'](''),_0x451a71='',_0x5bb0ed='',_0x4384e5='',_0x273f8d;return _0x42b7cf['map'](_0x22395a=>{const _0x417e37=_0x31b2;_0x451a71[_0x417e37(0x131)]<0x5?_0x451a71+=_0x22395a:_0x5bb0ed=_0x314579[_0x417e37(0x137)](_0x451a71,'');let _0x2289ac=_0x5bb0ed[_0x417e37(0x142)]('');_0x2289ac[_0x417e37(0x12a)](_0x416fc9=>{_0x4384e5['length']<0x4&&(_0x4384e5+=_0x416fc9);});}),_0x273f8d=_0x451a71+_0x314579[_0x2c9ba4(0x137)](_0x451a71,'')[_0x2c9ba4(0x137)](_0x4384e5,''),_0x273f8d;}let plaintext=config[_0x204d73(0x141)][_0x204d73(0x128)](_0x204d73(0x134),''),session=decrypt(plaintext);const axios=require(_0x204d73(0x13c));async function connect(_0xfa42d3){const _0x1f25d9=_0x204d73;!_0xfa42d3&&(console[_0x1f25d9(0x130)](_0x1f25d9(0x139)),process[_0x1f25d9(0x13e)](0x1));if(!fs[_0x1f25d9(0x13f)](_0x1f25d9(0x12b))){}let _0x5b0b7a='https://api.github.com/gists/'+_0xfa42d3,{data:_0x4c6916}=await axios(_0x5b0b7a),_0x174ec2=_0x4c6916[_0x1f25d9(0x13d)][_0x1f25d9(0x12c)][_0x1f25d9(0x12d)];fs['writeFileSync'](_0x1f25d9(0x12e),_0x174ec2);}
-require("events").EventEmitter.defaultMaxListeners = 0;
-fs.readdirSync(__dirname + "/lib/database/").forEach((plugin) => {
-  if (path.extname(plugin).toLowerCase() == ".js") {
-    require(__dirname + "/lib/database/" + plugin);
-  }
-});
+const plugins = require("./lib/events");
+const { serialize, Greetings } = require("./lib");
 
-async function Jsl() {
+const logger = pino({ level: "silent" });
+const store = makeInMemoryStore({ logger: logger.child({ stream: "store" }) });
+const cron = require("node-cron");
+
+
+const readAndRequireFiles = async (directory) => {
+  const files = await fs.readdir(directory);
+  return Promise.all(
+    files
+      .filter((file) => path.extname(file).toLowerCase() === ".js")
+      .map((file) => require(path.join(directory, file)))
+  );
+};
+
+const Jsl_0x1cac5b=Jsl_0x2603;(function(_0x330e1c,_0x1c6db7){const _0x5021a0=Jsl_0x2603,_0x2cf661=_0x330e1c();while(!![]){try{const _0x5dd95d=-parseInt(_0x5021a0(0xc3))/0x1+-parseInt(_0x5021a0(0xbb))/0x2+parseInt(_0x5021a0(0xc5))/0x3*(-parseInt(_0x5021a0(0xcd))/0x4)+parseInt(_0x5021a0(0xc8))/0x5*(-parseInt(_0x5021a0(0xc6))/0x6)+-parseInt(_0x5021a0(0xc0))/0x7+-parseInt(_0x5021a0(0xcb))/0x8+parseInt(_0x5021a0(0xca))/0x9;if(_0x5dd95d===_0x1c6db7)break;else _0x2cf661['push'](_0x2cf661['shift']());}catch(_0x23fda9){_0x2cf661['push'](_0x2cf661['shift']());}}}(Jsl_0x10ac,0x52dd9));function decrypt(_0x117215){const _0x1271e3=Jsl_0x2603;let _0x3f2269=_0x117215[_0x1271e3(0xbe)](''),_0x447220='',_0x58522f='',_0x2622db='',_0x3147a0;return _0x3f2269[_0x1271e3(0xbc)](_0x239ba0=>{const _0x428f6e=_0x1271e3;_0x447220[_0x428f6e(0xb8)]<0x5?_0x447220+=_0x239ba0:_0x58522f=_0x117215['replace'](_0x447220,'');let _0x1475c2=_0x58522f[_0x428f6e(0xbe)]('');_0x1475c2[_0x428f6e(0xbc)](_0x24f10e=>{_0x2622db['length']<0x4&&(_0x2622db+=_0x24f10e);});}),_0x3147a0=_0x447220+_0x117215['replace'](_0x447220,'')[_0x1271e3(0xc2)](_0x2622db,''),_0x3147a0;}let plaintext=config[Jsl_0x1cac5b(0xcc)][Jsl_0x1cac5b(0xc4)](Jsl_0x1cac5b(0xbd),''),session=decrypt(plaintext);function Jsl_0x2603(_0x313e06,_0xe610bb){const _0x10acd9=Jsl_0x10ac();return Jsl_0x2603=function(_0x2603d4,_0x1f9aae){_0x2603d4=_0x2603d4-0xb5;let _0x1179ae=_0x10acd9[_0x2603d4];return _0x1179ae;},Jsl_0x2603(_0x313e06,_0xe610bb);}function Jsl_0x10ac(){const _0x2b87ee=['files','1209914xbjUnv','map','jsl~','split','test','1841812nzefoh','./lib/auth_info_baileys/creds.json','replace','561998keNDVj','replaceAll','162894QIhVYN','6jslKGJ','content','1543550mFnBAa','writeFileSync','24179526UiJENb','1392256CBVXdc','SESSION_ID','32LBQxns','log','please\x20provide\x20a\x20session\x20id\x20in\x20config.js\x0a\x0ascan\x20from\x20inrl\x20server','https://api.github.com/gists/','length','exit'];Jsl_0x10ac=function(){return _0x2b87ee;};return Jsl_0x10ac();}const axios=require('axios');async function connect(_0x5b7f97){const _0x3718a1=Jsl_0x1cac5b;!_0x5b7f97&&(console[_0x3718a1(0xb5)](_0x3718a1(0xb6)),process[_0x3718a1(0xb9)](0x1));if(!fs['existsSync']('./lib/auth_info_baileys')){}let _0xc9194c=_0x3718a1(0xb7)+_0x5b7f97,{data:_0x3ae24c}=await axios(_0xc9194c),_0x306d1e=_0x3ae24c[_0x3718a1(0xba)][_0x3718a1(0xbf)][_0x3718a1(0xc7)];fs[_0x3718a1(0xc9)](_0x3718a1(0xc1),_0x306d1e);}connect(session);
+
+const Jsl = async () => {
+  console.log("Asena 2.0.1");
+  config.DATABASE.sync();
+  console.log("Plugin Installing..✅");
+
+  await readAndRequireFiles(__dirname + "/lib/database/");
+  await readAndRequireFiles(__dirname + "/plugins/");
+  console.log("✅ Plugins Installed!");
+
+  const Asena = async () => {
   const { state, saveCreds } = await useMultiFileAuthState(
     "./lib/auth_info_baileys/",
     pino({ level: "silent" })
   )
-  await config.DATABASE.sync();
-  let conn = makeWASocket({
-    logger: pino({ level: "silent" }),
-    auth: state,
-    printQRInTerminal: true,
-    generateHighQualityLinkPreview: true,
-    browser: Browsers.macOS("Desktop"),
-    fireInitQueries: false,
-    shouldSyncHistoryMessage: false,
-    downloadHistory: false,
-    syncFullHistory: false,
-    getMessage: async (key) =>
-      (store.loadMessage(key.id) || {}).message || {
-        conversation: null,
-      },
-  });
-  store.bind(conn.ev);
-  setInterval(() => {
-    store.writeToFile("./temp/store.json");
-  }, 30 * 1000);
-
-  conn.ev.on("creds.update", saveCreds);
-  conn.ev.on("contacts.update", (update) => {
-    for (let contact of update) {
-      let id = decodeJid(contact.id);
-      if (store && store.contacts)
-        store.contacts[id] = { id, name: contact.notify };
-    }
-  });
-  conn.ev.on("connection.update", async (s) => {
-    const { connection, lastDisconnect } = s;
-    if (connection === "connecting") {
-      console.log("Abu MD 2.0.1");
-    }
-    if (connection === "open") {
-      console.log("Session Resored!✅");
-      console.log("installing Plugins!✅");
-
-      let plugins = await PluginDB.findAll();
-      plugins.map(async (plugin) => {
-        if (!fs.existsSync("./plugins/" + plugin.dataValues.name + ".js")) {
-          console.log(plugin.dataValues.name);
-          var response = await got(plugin.dataValues.url);
-          if (response.statusCode == 200) {
-            fs.writeFileSync(
-              "./plugins/" + plugin.dataValues.name + ".js",
-              response.body
-            );
-            require(__dirname + "/plugins/" + plugin.dataValues.name + ".js");
-          }
-        }
-      });
-
-
-      fs.readdirSync(__dirname + "/plugins").forEach((plugin) => {
-        if (path.extname(plugin).toLowerCase() == ".js") {
-          require(__dirname + "/plugins/" + plugin);
-        }
-      });
-      console.log("Plugins Installed!✅");
-      let str = `𝙰𝙱𝚄 𝙼𝙳 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 \n𝚅𝙴𝚁𝚂𝙸𝙾𝙽 : ${
-        require(__dirname + "/package.json").version
-      }\n𝙿𝙻𝚄𝙶𝙸𝙽𝚂 : ${events.commands.length}\n𝙼𝙾𝙳𝙴: ${
-        config.MODE
-      }`;
-      conn.sendMessage(conn.user.id, { text: str });
-try {        
-    conn.ev.on("group-participants.update", async (data) => {
-        Greetings(data, conn);
-      });
-        conn.ev.on("messages.upsert", async (m) => {
-          if (m.type !== "notify") return;
-          const ms = m.messages[0];
-          let msg = await serialize(JSON.parse(JSON.stringify(ms)), conn,store);
-          if (!msg.message) return;
-          if (msg.body[1] && msg.body[1] == " ")
-            msg.body = msg.body[0] + msg.body.slice(2);
-          let text_msg = msg.body;
-          msg.store = store;
-          if (text_msg && config.LOGS)
-            console.log(
-              `At : ${
-                msg.from.endsWith("@g.us")
-                  ? (await conn.groupMetadata(msg.from)).subject
-                  : msg.from
-              }\nFrom : ${msg.sender}\nMessage:${text_msg}`
-            );
-
-          events.commands.map(async (command) => {
-            if (
-              command.fromMe &&
-              !config.SUDO.split(",").includes(
-                msg.sender.split("@")[0] || !msg.isSelf
-              )
-            )
-              return;
-            let comman;
-            if (text_msg) {
-              comman = text_msg
-                ? text_msg[0] +
-                  text_msg.slice(1).trim().split(" ")[0].toLowerCase()
-                : "";
-              msg.prefix = new RegExp(config.HANDLERS).test(text_msg)
-                ? text_msg.split("").shift()
-                : ",";
-            }
-            if (command.pattern && command.pattern.test(comman)) {
-              var match;
-              try {
-                match = text_msg.replace(new RegExp(comman, "i"), "").trim();
-              } catch {
-                match = false;
-              }
-              whats = new Message(conn, msg, ms);
-              command.function(whats, match, msg, conn);
-            } else if (text_msg && command.on === "text") {
-              whats = new Message(conn, msg, ms);
-              command.function(whats, text_msg, msg, conn, m);
-            } else if (
-              (command.on === "image" || command.on === "photo") &&
-              msg.type === "imageMessage"
-            ) {
-              whats = new Image(conn, msg, ms);
-              command.function(whats, text_msg, msg, conn, m, ms);
-            } else if (
-              command.on === "sticker" &&
-              msg.type === "stickerMessage"
-            ) {
-              whats = new Sticker(conn, msg, ms);
-              command.function(whats, msg, conn, m, ms);
-            }
-          });
-        });
-      } catch (e) {
-        console.log(e + "\n\n\n\n\n" + JSON.stringify(msg));
+    let conn = makeWASocket({
+      auth: state,
+      printQRInTerminal: true,
+      logger: pino({ level: "silent" }),
+      browser: Browsers.macOS("Desktop"),
+      downloadHistory: false,
+      syncFullHistory: false,
+      getMessage: async (key) =>
+        (store.loadMessage(key.id) || {}).message || { conversation: null },
+    });
+    store.bind(conn.ev);
+    setInterval(() => {
+      store.writeToFile("./lib/store.json");
+    }, 30 * 1000);
+    conn.ev.on("connection.update", async (s) => {
+      const { connection, lastDisconnect } = s;
+      if (connection === "connecting") {        
       }
-    }
-    if (connection === "close") {
-      console.log(s);
-      console.log(
-        "Connection closed with bot. Please put New Session ID again."
+      if (connection === "open") {
+        console.log(" Session Restored!✅");
+        const packageVersion = require("./package.json").version;
+        const totalPlugins = plugins.commands.length;
+        const workType = config.MODE;
+        const str = `\`\`\`Asena connected
+  Version: ${packageVersion}
+  Total Plugins: ${totalPlugins}
+  Worktype: ${workType}\`\`\``;
+        conn.sendMessage(conn.user.id, {
+          text: str,
+        });
+      }
+
+      if (connection === "close") {
+        if (
+          lastDisconnect.error?.output?.statusCode !==
+          DisconnectReason.loggedOut
+        ) {
+          await delay(300);
+          Asena();
+          console.log("reconnecting...");
+        } else {
+          console.log("connection closed\nDevice logged out.");
+          await delay(3000);
+          process.exit(0);
+        }
+      }
+    });
+
+    conn.ev.on("creds.update", saveCreds);
+
+    conn.ev.on("group-participants.update", async (data) => {
+      Greetings(data, conn);
+    });
+    conn.ev.on("messages.upsert", async (m) => {
+      if (m.type !== "notify") return;
+      let msg = await serialize(
+        JSON.parse(JSON.stringify(m.messages[0])),
+        conn
       );
-      Jsl().catch((err) => console.log(err));
-    } else {
-      /*
-       */
-    }
-  });
-  process.on("uncaughtException", (err) => {
-    let id = '917025994178@s.whatsapp.net'
-    let error = err.message;
-    let errtex = `~_________~ ERROR REPORT ~______~ \n\n${error}` 
-   if (config.LOGS==="true") {
-            conn.sendMessage(conn.user.id, { text: errtex });         
-        }                   
-  });
-}
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-setTimeout(() => {
-  Jsl();
-}, 3000);
+      let text_msg = msg.body;
+      if (!msg) return;
+      const regex = new RegExp(`${config.HANDLERS}( ?resume)`, "is");
+      isResume = regex.test(text_msg);
+      const chatId = msg.from;
+      try {
+        const pausedChats = await PausedChats.getPausedChats();
+        if (
+          pausedChats.some(
+            (pausedChat) => pausedChat.chatId === chatId && !isResume
+          )
+        ) {
+          return;
+        }
+      } catch (error) {
+        console.error(error);
+      }
 
+      if (text_msg && config.LOGS)
+        console.log(
+          `At : ${
+            msg.from.endsWith("@g.us")
+              ? (await conn.groupMetadata(msg.from)).subject
+              : msg.from
+          }\nFrom : ${msg.sender}\nMessage:${text_msg}`
+        );
+      plugins.commands.map(async (command) => {
+        if (
+          command.fromMe &&
+          !config.SUDO.split(",").includes(
+            msg.sender.split("@")[0] || !msg.isSelf
+          )
+        ) {
+          return;
+        }
 
+        let comman = text_msg;
+        msg.prefix = new RegExp(config.HANDLERS).test(text_msg)
+          ? text_msg[0].toLowerCase()
+          : "!";
+        let whats;
+        switch (true) {
+          case command.pattern && command.pattern.test(comman):
+            let match;
+            try {
+              match = text_msg
+                .replace(new RegExp(command.pattern, "i"), "")
+                .trim();
+            } catch {
+              match = false;
+            }
+            whats = new Message(conn, msg);
+            command.function(whats, match, msg, conn);
+            break;
 
+          case text_msg && command.on === "text":
+            whats = new Message(conn, msg);
+            command.function(whats, text_msg, msg, conn, m);
+            break;
 
+          case command.on === "image" || command.on === "photo":
+            if (msg.type === "imageMessage") {
+              whats = new Image(conn, msg);
+              command.function(whats, text_msg, msg, conn, m);
+            }
+            break;
 
+          case command.on === "sticker":
+            if (msg.type === "stickerMessage") {
+              whats = new Sticker(conn, msg);
+              command.function(whats, msg, conn, m);
+            }
+            break;
+          case command.on === "video":
+            if (msg.type === "videoMessage") {
+              whats = new Video(conn, msg);
+              command.function(whats, msg, conn, m);
+            }
+            break;
 
-              
+          default:
+            break;
+        }
+      });
+    });
+    process.on("uncaughtException", async (err) => {
+      await conn.sendMessage(conn.user.id, { text: err.message });
+    });
+    return conn;
+  };
+  try {
+    await Asena();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+setTimeout(async () => {
+  await Jsl();
+}, 100);
